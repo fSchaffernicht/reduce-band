3053597e199f6c786432c13b2adfa22be1ef067a
Felix Schaffernicht <felix@schaffernicht.com>
2018-04-24 23:10:03 +0200

    change config for markdown

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import styled, { ThemeProvider } from 'styled-components'
-
 import theme from './theme'
 
 import Header from '../components/header'
 import './index.css'
-console.log('theme', theme)
+
 const Wrapper = styled.div`
-  background: ${props => props.theme.color.background};
+  margin-top: 300px;
+`
+
+const Container = styled.div`
+  margin: auto;
+  max-width: 960px;
+  padding: 0 0.5rem;
 `
 
 const Layout = ({ children, data }) => (
@@ -24,16 +29,9 @@ const Layout = ({ children, data }) => (
         ]}
       />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0px 1.0875rem 1.45rem',
-          paddingTop: 0
-        }}
-      >
+      <Container>
         {children()}
-      </div>
+      </Container>
     </Wrapper>
   </ThemeProvider>
 )
