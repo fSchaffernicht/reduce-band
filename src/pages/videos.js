abf6052e8374b93cd5cf8711aec5b3163ee297a6
Felix Schaffernicht <felix@schaffernicht.com>
2018-05-24 23:09:16 +0200

    change key

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -9,7 +9,7 @@ import {
 
 import styled from 'styled-components';
 
-const key = process.env.API_KEY
+const key = API_KEY
 console.log(key)
 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCCxaY-87Cazvpq7AIqWW8nA&key=${key}`
 
